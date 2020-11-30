ee846795a54c983a4cb95b55325142eba172c724
Sean Hanauer <yodaman40@gmail.com>
2020-11-30 16:25:04 -0500

    now the extension changes the word shell to pearl. I'm doing this so that I can modify it in the future so that I can do searches for differnt words.

--- content.js
+++ content.js
@@ -1,23 +1,24 @@
 // content.js
 
-// chrome.runtime.onMessage.addListener(
-// 	function(request, sender, sendResponse) {
-// 	  if( request.message === "clicked_browser_action" ) {
-// 		var firstHref = $("a[href^='http']").eq(0).attr("href");
-  
-// 		console.log(firstHref);
-// 		alert("Hello from your chrome extension!");
-// 		}
-// 	}
-// );
-
-// var pn = 
-// const re = new RegExp('Not', 'gi')
-// const matches = document.documentElement.innerHTML.match(re)
-// chrome.runtime.sendMessage({
-//     url: window.location.href,
-//     count: matches.length
-// })
-
-
 alert("Hello browser!")
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/shell/gi, 'pearl');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+
+
